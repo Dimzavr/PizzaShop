a4cf6ab6741d79719518eadc5bdccbe5c7d2e6b4
Dimzavr <ndim41@gmail.com>
2020-02-18 14:04:24 +0300

    Display products,step1

---
+++ db/migrate/20200123144333_add_products.rb
@@ -0,0 +1,35 @@
+class AddProducts < ActiveRecord::Migration[6.0]
+  def change
+  	Product.create ({
+  		:title => 'Hawaiian',
+  		:description => 'This is Hawaiian pizza', 
+  		:price => 350, 
+  		:size => 30, 
+  		:is_spicy => false, 
+  		:is_veg => false, 
+  		:is_best_offer => false, 
+  		:path_to_image => '/images/1.jpg'
+  		})
+
+  	Product.create ({
+  		:title => 'Pepperoni',
+  		:description => 'Nice pepperoni pizza',
+  		:price => 400,
+  		:size => 30,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => true,
+  		:path_to_image => '/images/2.jpg'})
+
+  	Product.create ({
+  		:title => 'Vegitarian', 
+  		:description => 'Amazing Vegitarian pizza', 
+  		:price => 400, 
+  		:size => 30, 
+  		:is_spicy => false, 
+  		:is_veg => false, 
+  		:is_best_offer => false, 
+  		:path_to_image => '/images/3.jpg'})
+
+  end
+end
