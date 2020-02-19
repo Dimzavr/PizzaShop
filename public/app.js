c5157c5f2fdaecbfc8435825a10587cf04429481
Dimzavr <ndim41@gmail.com>
2020-02-19 12:11:58 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,17 +1,16 @@
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id);
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key); // получаем значение по id
+	x = x * 1 + 1; 
+	window.localStorage.setItem(key, x);
 }
-
 function something()
 {
 
 	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 	x = x * 1 + 1; // x = x + 1
-	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x set hash value
 	alert(x);
 	
 }
-
-
-
